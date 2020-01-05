9d6a51089f3191b42456282da11c4a0446766b4f
Pavel Krotov <pavel@krotov.us>
2020-01-04 20:42:00 -0500

    initial commit

---
+++ app/db/data.js
@@ -0,0 +1,347 @@
+;var RESULTS = {
+    "featured": "Wellness Center",
+    "projects": [
+        {
+            "name": "Wellness Center",
+            "thumb": "db/img/a03.jpg",
+            "desc": "Angular-based front end for the wellness center",
+            "url": "https://wellness-site.firebaseapp.com/#/contact",
+            "src": "https://wellness-site.firebaseapp.com/",
+            "tags": [
+                "Website",
+                "Angular",
+                "Bootstrap",
+                "Firebase",
+                "JSON"
+            ]
+        },
+        {
+            "name": "Art Gallery",
+            "thumb": "db/img/a07.jpg",
+            "desc": "single page with ui-routing and json for the Cuban-American artist Mario Torroella.",
+            "url": "http://pavelkrotov.com/mario-site/",
+            "src": "https://github.com/pasha1307/mario_art.git",
+            "tags": [
+                "Website",
+                "Angular",
+                "Animation",
+                "JSON"
+            ]
+        },
+        {
+            "name": "PSF: Theme 1",
+            "thumb": "db/img/a02.jpg",
+            "desc": "Custom themes for Joomla-based web site. Pitirim A. Sorokin Foundation",
+            "url": "http://sorokinfoundation.org",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "Joomla",
+                "CMS",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "PSF: Theme 2",
+            "thumb": "db/img/a06.jpg",
+            "desc": "Custom themes for Joomla-based web site. Pitirim A. Sorokin Foundation",
+            "url": "http://cliffstreet.org/index.php/home",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "Joomla",
+                "CMS",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "PSF: Theme 3",
+            "thumb": "db/img/a14.jpg",
+            "desc": "Custom themes for Joomla-based web site. Pitirim A. Sorokin Foundation",
+            "url": "http://cliffstreet.org/index.php/foundation",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "Joomla",
+                "CMS",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "PSF: Theme 4",
+            "thumb": "db/img/a15.jpg",
+            "desc": "Custom themes for Joomla-based web site. Pitirim A. Sorokin Foundation",
+            "url": "http://cliffstreet.org/index.php/theories",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "Joomla",
+                "CMS",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "PSF: Theme 5",
+            "thumb": "db/img/a11.jpg",
+            "desc": "Custom themes for Joomla-based web site. Pitirim A. Sorokin Foundation",
+            "url": "http://cliffstreet.org/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "Joomla",
+                "CMS",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "PSF: Elastica",
+            "thumb": "db/img/a05.jpg",
+            "desc": "custom made Joomla theme in Responsive mode Elastica T1",
+            "url": "http://pitirim.org/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "Joomla",
+                "CMS",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "MARC",
+            "thumb": "db/img/a04.jpg",
+            "desc": "corporate site for the market research in two languages",
+            "url": "http://www.marc-analytics.com",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "Website",
+                "HTML",
+                "CSS",
+                "JQuery",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "Drevnosti",
+            "thumb": "db/img/a13.jpg",
+            "desc": "the site for the publishing company",
+            "url": "http://drevnostisevera.ru",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "website",
+                "HTML",
+                "CSS",
+                "JQuery",
+                "Custom themes"
+            ]
+        },
+        {
+            "name": "Site: Saskatoon",
+            "thumb": "db/img/b09.jpg",
+            "desc": "design and wireframe for Drupal",
+            "url": "http://library2.usask.ca/sorokin/about",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "website",
+                "Drupal",
+                "CMS"
+            ]
+        },
+        {
+            "name": "Angular Quiz",
+            "thumb": "db/img/a10.jpg",
+            "desc": "Angular and CSS quiz app",
+            "url": "https://pitirim-quiz.firebaseapp.com",
+            "src": "https://github.com/pasha1307/angular-quiz.git",
+            "tags": [
+                "webapps",
+                "Angular",
+                "CSS"
+            ]
+        },
+        {
+            "name": "Catalogue CRUD",
+            "thumb": "db/img/a08.jpg",
+            "desc": "Angular with JSON and Firebase for presenting and CRUD large data; ui-Semantic",
+            "url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "webapps",
+                "Angular",
+                "Firebase",
+                "JSON",
+                "ui-semantic"
+            ]
+        },
+        {
+            "name": "Strongloop API: Angular CRUD",
+            "thumb": "db/img/b11.jpg",
+            "desc": "CRUD app with Backloop.io and MongoDb and Angular on the front end; materialize.css",
+            "url": "http://ppkstudio.cloud",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "webapps",
+                "Angular",
+                "Loopback",
+                "MongoDB",
+                "materilize"
+            ]
+        },
+        {
+            "name": "Firebase Chat",
+            "thumb": "db/img/a12.jpg",
+            "desc": "example of three-way data binding in Firebase chat",
+            "url": "http:ppk.firebaseapp.com",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "webapps",
+                "Angular",
+                "Firebase",
+                "JSON"
+            ]
+        },
+        {
+            "name": "Yagel Moss",
+            "thumb": "db/img/a16.jpg",
+            "desc": "From Features of the North",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "photo",
+                "Adobe Creative Suite",
+                "Photoshop"
+            ]
+        },
+        {
+            "name": "A Priest",
+            "thumb": "db/img/a17.jpg",
+            "desc": "From Features of the North",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "photo",
+                "Adobe Creative Suite",
+                "Photoshop"
+            ]
+        },
+        {
+            "name": "Logo for Wellness",
+            "thumb": "db/img/a01.jpg",
+            "desc": "basic logo for the Winchester Wellness Center",
+            "url": "https://wellness-site.firebaseapp.com/#/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite"
+            ]
+        },
+        {
+            "name": "Sport Logo",
+            "thumb": "db/img/b01.jpg",
+            "desc": "Adobe I/Ph made company logo for the Sochi Olompics",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite",
+                "Adobe Illustrator"
+            ]
+        },
+        {
+            "name": "Company Logo",
+            "thumb": "db/img/b04.jpg",
+            "desc": "logo for business cards",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite",
+                "Adobe Illustrator"
+            ]
+        },
+        {
+            "name": "Logo: Publisher",
+            "thumb": "db/img/b02.jpg",
+            "desc": "logo for the book publishing company",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite",
+                "Adobe Illustrator"
+            ]
+        },
+        {
+            "name": "PPK Studio",
+            "thumb": "db/img/b12.jpg",
+            "desc": "logo for the UI-UX company",
+            "url": "http://ppkstudio.cloud/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite",
+                "Adobe Illustrator"
+            ]
+        },
+        {
+            "name": "UI-UX",
+            "thumb": "db/img/b14.png",
+            "desc": "logo for the UI-UX company",
+            "url": "http://ppkstudio.cloud/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite",
+                "Adobe Illustrator"
+            ]
+        },
+        {
+            "name": "Gates to the village",
+            "thumb": "db/img/b06.jpg",
+            "desc": "entering the village in the Northern Russia",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "photo",
+                "Adobe Creative Suite",
+                "Adobe Photoshop"
+            ]
+        },
+        {
+            "name": "Portrait",
+            "thumb": "db/img/b07.jpg",
+            "desc": "features of the North",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "photo",
+                "Adobe Creative Suite",
+                "Adobe Photoshop"
+            ]
+        },
+        {
+            "name": "Portrait",
+            "thumb": "db/img/b06.jpg",
+            "desc": "Dr.Sergei P. Sorokin",
+            //"url": "http://angular.github.com/angular-phonecat/step-11/app/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "photo",
+                "Adobe Creative Suite",
+                "Adobe Photoshop"
+            ]
+        },
+        {
+            "name": "Various Logos",
+            "thumb": "db/img/b08.jpg",
+            "desc": "basic logo for the Winchester Wellness Center",
+            //"url": "https://wellness-site.firebaseapp.com/#/",
+            //"src": "http://docs.angularjs.org/tutorial",
+            "tags": [
+                "graphic",
+                "Adobe Creative Suite"
+            ]
+        }
+
+    ]
+
+};
